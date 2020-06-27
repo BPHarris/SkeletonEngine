9f6423cc9f86aa61bdd28943fb368fdf14d579a0
bpharris <b.harris.4@warwick.ac.uk>
2020-06-27 23:15:36 +0100

    Added assertions

--- SkeletonEngine/src/SkeletonEngine/Core.h
+++ SkeletonEngine/src/SkeletonEngine/Core.h
@@ -1,6 +1,20 @@
 #pragma once
 
 
+#define BIT(x) (1 << x)
+
+
+#ifdef SE_ENABLE_ASSERTS
+	#define SE_ASSERT(x, ...) { if(!(x)) { SE_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
+	#define SE_CORE_ASSERT(x, ...) { if(!(x)) { SE_CORE_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
+#else
+	#define SE_ASSERT(x, ...)
+	#define SE_CORE_ASSERT(x, ...)
+#endif // SE_ENABLE_ASSERTS
+
+
+
+/* SharedLib/DLL declspecs */
 #ifdef SE_PLATFORM_WINDOWS
 	#ifdef SE_BUILD_DLL
 		#define SE_API __declspec(dllexport)
@@ -10,6 +24,3 @@
 #else
 	#error SkeletonEngine only supports windows, thus far.
 #endif // SE_PLATFORM_WINDOWS
-
-
-#define BIT(x) (1 << x)
