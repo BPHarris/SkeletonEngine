ab5b4e6f682b5058bad985815e7c8409117480af
bpharris <b.harris.4@warwick.ac.uk>
2020-07-15 00:29:11 +0100

    Redo V1

--- SkeletonEngine/src/SkeletonEngine/Events/Event.h
+++ SkeletonEngine/src/SkeletonEngine/Events/Event.h
@@ -11,91 +11,62 @@
 #include "sepch.h"
 #include "SkeletonEngine/Core.h"
 
+
 namespace SkeletonEngine
 {
-
-    /* Events in the SkeletonEngine are currently blocking.
-     * 
-     * At some point this should be changed to be buffered and processed in the
-     * event phase of the update stage.
-     */
+    // NOTE: Events in the SkeletonEngine are currently blocking.
+    // TODO: Buffer events and process in event phase of update.
+	
 
     /** SkeletonEngine EventType. */
     enum class EventType
     {
         None = 0,
-
-        WindowClosed,           /**< Window closed. */
-        WindowResized,          /**< Window resized. */
-        WindowFocused,          /**< Window gained focused. */
-        WindowLostFocus,        /**< Window lost focus. */
-        WindowMoved,            /**< Window moved. */
-
-        AppTick,                /**< Application tick. */
-        AppUpdate,              /**< Application update. */
-        AppRender,              /**< Application render. */
-
-        KeyPressed,             /**< Key pressed (or repeated). */
-        KeyReleased,            /**< Key released. */
-
-        MouseButtonPressed,     /**< Mouse clicked/mouse button pressed. */
-        MouseButtonReleased,    /**< Mouse unclicked/mouse button released. */
-        MouseMoved,             /**< Mouse moved. */
-        MouseScrolled           /**< Mouse scolled up/down. */
+        WindowClosed, WindowResized, WindowFocused, WindowLostFocus, WindowMoved,
+        AppTick, AppUpdate, AppRender,
+        KeyPressed, KeyReleased,
+        MouseButtonPressed, MouseButtonReleased, MouseMoved, MouseScrolled,
     };
 
-    /** SkeletonEngine EventCategory */
+
+    /** SkeletonEngine EventCategory, not mutually exclusive. */
     enum EventCategory
     {
         None = 0,
-        EventCategoryApplication = BIT(0),  /**< Application events */
+        EventCategoryApplication = BIT(0),  /**< Application/window events */
         EventCategoryInput = BIT(1),        /**< Input events */
         EventCategoryKeyboard = BIT(2),     /**< Keyboard events */
         EventCategoryMouse = BIT(3),        /**< Mouse events */
         EventCategoryMouseButton = BIT(4)   /**< Mouse button events */
     };
 
-#define EVENT_CLASS_TYPE(type)                                                  \
-    static EventType GetStaticType() { return EventType::##type; }              \
-    virtual EventType GetEventType() const override { return GetStaticType(); } \
-    virtual const char *GetName() const override { return #type; }
 
-#define EVENT_CLASS_CATEGORY(category) \
-    virtual int GetCategoryFlags() const override { return category; }
+#define EVENT(event_type, category)     Event(EventType::event_type, #event_type, category)
+#define EVENT_STATIC_TYPE(event_type)   static EventType GetStaticType() { return EventType::event_type; }
 
-    /** SkeletonEngine Event class. */
+
+    /** SkeletonEngine Event "interface". */
     class SE_API Event
     {
         friend class EventDispatcher;
 
     public:
-        bool Handled = false;
+        bool is_handled = false;
 
-    public:
-        /** @return    The Event's EventType. */
-        virtual EventType GetEventType() const = 0;
+        EventType type;
+        std::string name;
+        int category_flags;
 
-        /** @return    The Event name. */
-        virtual const char *GetName() const = 0;
+        Event(EventType type, std::string name, int category_flags)
+            : type(type), name(name), category_flags(category_flags) {}
 
-        /** @return    The Event's GetCategoryFlags. */
-        virtual int GetCategoryFlags() const = 0;
+        /** @return true if the Event is in the given EventCategory, false otherwise. */
+        inline bool IsInCategory(EventCategory category) { return category_flags & category; }
 
-        /** @return    The string representation of the Event. */
-        virtual std::string ToString() const { return GetName(); }
-
-        /**
-         * Check if this Event is in the given category.
-         * 
-         * @param   category    The category to check against.
-         * @return              true if the Event is in the the EventCategory
-         * @return              false otherwise
-         */
-        inline bool IsInCategory(EventCategory category)
-        {
-            return GetCategoryFlags() & category;
-        }
+        /** @return string representation of the Event. */
+        virtual std::string ToString() const { return name; }
     };
+
 
     /** SkeletonEngine EventDispatcher template class. */
     class EventDispatcher
@@ -104,30 +75,31 @@ namespace SkeletonEngine
         using EventFn = std::function<bool(T &)>;
 
     private:
-        Event &m_Event;
+        Event &event;
 
     public:
-        EventDispatcher(Event &event) : m_Event(event) {}
+        EventDispatcher(Event &event) : event(event) {}
 
         template <typename T>
         bool Dispatch(EventFn<T> func)
         {
-            if (m_Event.GetEventType() == T::GetStaticType())
+            if (event.type == T::GetStaticType())
             {
-                m_Event.Handled = func(*(T *)&m_Event);
+                event.is_handled = func(*(T *)&event);
                 return true;
             }
             return false;
         }
     };
 
+
     /**
-     * Operator overload for << and Event.
-     * 
-     * @param os    Left-hand side.
-     * @param e     An Event instance.
-     * @return      String concatination of os and e.
-     */
+	 * Operator overload for << and Event.
+	 * 
+	 * @param os    Left-hand side.
+	 * @param e     An Event instance.
+	 * @return      String concatination of os and e.
+	 */
     inline std::ostream &operator<<(std::ostream &os, const Event &e)
     {
         return os << e.ToString();
